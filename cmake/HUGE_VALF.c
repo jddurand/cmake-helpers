d8cf6ae4bb58f8e590130862148c087781930a70
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-12 14:51:10 +0100

    dev+

---
+++ cmake/HUGE_VALF.c
@@ -0,0 +1,20 @@
+#cmakedefine HAVE_STDLIB_H
+#cmakedefine HAVE_MATH_H
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifdef HAVE_MATH_H
+#include <math.h>
+#endif
+
+#ifdef HAVE_HUGE_VALF_REPLACEMENT
+#  undef HUGE_VALF
+#  define HUGE_VALF (__builtin_huge_valf())
+#endif
+
+int main() {
+  float x = -C_HUGE_VALF;
+  return 0;
+}
---
+++ cmake/HUGE_VALL.c
@@ -0,0 +1,20 @@
+#cmakedefine HAVE_STDLIB_H
+#cmakedefine HAVE_MATH_H
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifdef HAVE_MATH_H
+#include <math.h>
+#endif
+
+#ifdef HAVE_HUGE_VALL_REPLACEMENT
+#  undef HUGE_VALL
+#  define HUGE_VALL (__builtin_huge_vall())
+#endif
+
+int main() {
+  long double x = -C_HUGE_VALL;
+  return 0;
+}
