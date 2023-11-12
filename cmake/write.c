6223c5116527fd4ca4171fd41f05d946b3b83f19
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-12 12:42:22 +0100

    dev+

--- cmake/write.c
+++ cmake/write.c
@@ -1,6 +1,7 @@
 #cmakedefine HAVE_STDIO_H
 #cmakedefine HAVE_STDLIB_H
 #cmakedefine HAVE_UNISTD_H
+#cmakedefine HAVE_FCNTL_H
 
 #ifdef HAVE_STDIO_H
 #include <stdio.h>
@@ -14,7 +15,24 @@
 #include <unistd.h>
 #endif
 
+#ifdef HAVE_FCNTL_H
+#include <fcntl.h>
+#endif
+
+#include <windows.h>
+
 int main() {
-  C_WRITE(stderr, "", 1);
+  /* C.f. http://codewiki.wikidot.com/c:system-calls:write */
+  int filedesc = open("testfile.txt", O_CREAT|O_WRONLY|O_TRUNC);;
+
+  if (filedesc < 0) {
+    exit(1);
+  }
+  if (C_WRITE(1, "This will be output to standard out\n", 36) != 36) {
+    exit(1);
+  }
+
+  close(filedesc);
+
   exit(0);
 }
