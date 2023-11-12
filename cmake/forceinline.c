7383806681828d146e35997f789d643559565f76
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-12 12:13:55 +0100

    dev+

--- cmake/forceinline.c
+++ cmake/forceinline.c
@@ -4,18 +4,13 @@
 #include <stdlib.h>
 #endif
 
-static C_FORCEINLINE foo_t static_foo();
-static foo_t foo();
 typedef int foo_t;
-
-int main(int argc, char *argv[]){
-  exit(0);
-}
-
 static C_FORCEINLINE foo_t static_foo() {
   return 0;
 }
-
-static foo_t foo() {
+foo_t foo() {
   return 0;
+}
+int main(int argc, char *argv[]){
+  exit(0);
 }
