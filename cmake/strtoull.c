a716b06a511e9de540e00c207ab048780e0df0e6
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-12 16:46:24 +0100

    dev+

--- cmake/strtoull.c
+++ cmake/strtoull.c
@@ -7,7 +7,7 @@
 #if defined(_MSC_VER) || defined(__BORLANDC__)
 /* Just because these compilers might not have long long, but they always have __int64. */
 /*   Note that on Windows short is always 2, int is always 4, long is always 4, __int64 is always 8 */
-#  define ULONG_LONG __uint64
+#  define ULONG_LONG unsigned __int64
 #else
 #  define ULONG_LONG unsigned long long
 #endif
