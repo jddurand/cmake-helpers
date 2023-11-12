bf5452e6fc3c3f0f0564eaeb6e1b36b3fb10a38a
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-12 12:17:13 +0100

    dev+

--- cmake/va_copy.c
+++ cmake/va_copy.c
@@ -18,10 +18,14 @@ int main() {
 
 static void f(int i, ...) {
   va_list args1, args2;
-  va_start (args1, i);
+
+  va_start(args1, i);
   C_VA_COPY(args2, args1);
-  if (va_arg (args2, int) != 42 || va_arg (args1, int) != 42) {
+
+  if (va_arg(args2, int) != 42 || va_arg(args1, int) != 42) {
     exit(1);
   }
-  va_end (args1); va_end (args2);
+
+  va_end(args1);
+  va_end(args2);
 }
