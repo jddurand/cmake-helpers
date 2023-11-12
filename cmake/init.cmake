d266713179fbe84aa97758fbbca59f230b85500a
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-12 13:50:26 +0100

    dev+

--- cmake/init.cmake
+++ cmake/init.cmake
@@ -92,6 +92,8 @@ function(cmake_helpers_init)
   cmake_helpers_try_run(C_LOG2F ${PROJECT_SOURCE_DIR}/cmake/log2f.c log2f)
   cmake_helpers_try_value(C_CHAR_BIT ${PROJECT_SOURCE_DIR}/cmake/CHAR_BIT.c CHAR_BIT)
   cmake_helpers_try_run(C_STRTOLD ${PROJECT_SOURCE_DIR}/cmake/strtold.c strtold _strtold __strtold)
+  cmake_helpers_try_run(C_STRTOD ${PROJECT_SOURCE_DIR}/cmake/strtod.c strtod _strtod __strtod)
+  cmake_helpers_try_run(C_STRTOF ${PROJECT_SOURCE_DIR}/cmake/strtof.c strtof _strtof __strtof)
   #
   # Check GNU features
   #
