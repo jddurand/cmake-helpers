dbfeb0d5d6752a6b0dabd2874802e7c580762a63
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-12 14:28:57 +0100

    dev+

---
+++ cmake/HUGE_VAL.c
@@ -0,0 +1,20 @@
+#cmakedefine HAVE_STDLIB_H
+#cmakedefine HAVE_MATH_H
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifdef HAVE_MATH_H
+#include <math.h>
+#endif
+
+#ifdef HAVE_HUGE_VAL_REPLACEMENT
+#  undef HUGE_VAL
+#  define HUGE_VAL (__builtin_huge_val())
+#endif
+
+int main() {
+  double x = -C_HUGE_VAL;
+  return 0;
+}
