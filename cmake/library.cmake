161c417f7dcfcbeed427d9227e7f6550e6664c28
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-19 09:22:25 +0100

    dev+

--- cmake/library.cmake
+++ cmake/library.cmake
@@ -459,7 +459,7 @@ endif()
 set(CMAKE_PREFIX_PATH "$ENV{CMAKE_MODULE_ROOT_PATH_ENV}")
 
 if(CMAKE_HELPERS_DEBUG)
-  message(STATUS "[pc.@_cmake_helpers_library_namespace@/build] Requiring @_cmake_helpers_library_namespace@")
+  message(STATUS "[pc.@_cmake_helpers_library_namespace@/build] Requiring @_cmake_helpers_library_namespace@ COMPONENTS LibraryComponent")
 endif()
 find_package(@_cmake_helpers_library_namespace@ REQUIRED COMPONENTS LibraryComponent)
 
