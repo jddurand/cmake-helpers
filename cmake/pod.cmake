b2472c539f6a4971f3c4e1adaee844c187fb63a6
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-20 07:33:24 +0100

    dev+

--- cmake/pod.cmake
+++ cmake/pod.cmake
@@ -66,64 +66,24 @@ function(cmake_helpers_pod)
   if(_cmake_helpers_pod_pod2man)
     find_program(GZIP gzip)
     if(GZIP)
-      if(NOT (TARGET cmake_helpers_doc_iface))
-	#
-	# We create an doc INTERFACE library, which we will depend on all document generator targets
-	#
-	cmake_helpers_call(add_library cmake_helpers_doc_iface INTERFACE)
-      endif()
-      if(NOT (TARGET cmake_helpers_pod_iface))
-	#
-	# We create an pod INTERFACE library, and make cmake_helpers_doc_iface depend on it
-	#
-	cmake_helpers_call(add_library cmake_helpers_pod_iface INTERFACE)
-	cmake_helpers_call(add_dependencies cmake_helpers_doc_iface cmake_helpers_pod_iface)
-      endif()
       #
-      # We create an INTERFACE library specific for this man page
-      #
-      set(_cmake_helpers_pod_target cmake_helpers_pod_${_cmake_helpers_pod_name})
-      cmake_helpers_call(add_library ${_cmake_helpers_pod_target} INTERFACE)
-      #
-      # We say that cmake_helpers_pod_iface depend on this target
-      #
-      cmake_helpers_call(add_dependencies cmake_helpers_pod_iface ${_cmake_helpers_pod_target})
-      #
-      # Add a custom command to do pod > man
+      # Create a custom target that generates the gzipped manpage
       #
       set(_cmake_helpers_pod_output "${CMAKE_CURRENT_BINARY_DIR}/${_cmake_helpers_pod_name}.${_cmake_helpers_pod_section}")
-      cmake_helpers_call(add_custom_command
-	OUTPUT ${_cmake_helpers_pod_output}
-	DEPENDS ${_cmake_helpers_pod_input}
-	USES_TERMINAL
-	COMMAND ${${_cmake_helpers_pod_pod2man}}
-	ARGS --section ${_cmake_helpers_pod_section} --center ${_cmake_helpers_library_namespace} -r ${_cmake_helpers_library_version} --stderr --name ${_cmake_helpers_pod_name} ${_cmake_helpers_pod_input} > ${_cmake_helpers_pod_output}
-      )
-      #
-      # Add a custom command to do man > man.gz
-      #
       set(_cmake_helpers_gzip_output "${CMAKE_CURRENT_BINARY_DIR}/${_cmake_helpers_pod_name}.${_cmake_helpers_pod_section}.gz")
-      cmake_helpers_call(add_custom_command
-	OUTPUT ${_cmake_helpers_gzip_output}
-	DEPENDS ${_cmake_helpers_pod_output}
+      set(_cmake_helpers_pod_target cmake_helpers_pod_${_cmake_helpers_pod_name})
+      add_custom_target(${_cmake_helpers_pod_target}
+	COMMAND ${${_cmake_helpers_pod_pod2man}} --section ${_cmake_helpers_pod_section} --center ${_cmake_helpers_library_namespace} -r ${_cmake_helpers_library_version} --stderr --name ${_cmake_helpers_pod_name} ${_cmake_helpers_pod_input} > ${_cmake_helpers_pod_output}
 	COMMAND ${GZIP} -c ${_cmake_helpers_pod_output} > ${_cmake_helpers_gzip_output}
+	DEPENDS ${_cmake_helpers_pod_input}
       )
-      #
-      # Add gzip output as a dependency
-      #
-      cmake_helpers_call(add_dependencies ${_cmake_helpers_pod_target} ${_cmake_helpers_gzip_output})
-      #
-      # Add the gzip GENERATED file to ${_cmake_helpers_pod_target} as a resource
-      #
-      cmake_helpers_call(set_target_properties ${_cmake_helpers_pod_target}
-	PROPERTIES RESOURCE ${_cmake_helpers_gzip_output})
       #
       # Install rules
       #
       cmake_helpers_call(install
-	TARGETS        ${_cmake_helpers_pod_target}
+	FILES          ${_cmake_helpers_gzip_output}
 	EXPORT         ${_cmake_helpers_library_namespace}DocumentationTargets
-	RESOURCE       DESTINATION ${CMAKE_INSTALL_MANDIR}/man${_cmake_helpers_pod_section}
+	DESTINATION    ${CMAKE_INSTALL_MANDIR}/man${_cmake_helpers_pod_section}
       )
       cmake_helpers_call(install
 	EXPORT ${_cmake_helpers_library_namespace}DocumentationTargets
