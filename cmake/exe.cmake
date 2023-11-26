5c424b5a77a663a9b5f87942d35c912f4c1a578c
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-11-26 05:38:48 +0100

    dev+

--- cmake/exe.cmake
+++ cmake/exe.cmake
@@ -116,7 +116,11 @@ function(cmake_helpers_exe name)
       #
       # A tiny hook to force ctest to build the executable
       #
-      cmake_helpers_call(add_test NAME ${_target}_build COMMAND "${CMAKE_COMMAND}" --build "${CMAKE_CURRENT_BINARY_DIR}" --target ${_target})
+      if(CTEST_CONFIGURATION_TYPE)
+	cmake_helpers_call(add_test NAME ${_target}_build COMMAND "${CMAKE_COMMAND}" --build "${CMAKE_CURRENT_BINARY_DIR}" --config "${CTEST_CONFIGURATION_TYPE}" --target ${_target})
+      else()
+	cmake_helpers_call(add_test NAME ${_target}_build COMMAND "${CMAKE_COMMAND}" --build "${CMAKE_CURRENT_BINARY_DIR}" --target ${_target})
+      endif()
       cmake_helpers_call(set_tests_properties ${_target}_test PROPERTIES DEPENDS ${_target}_build)
     endif()
   endforeach()
