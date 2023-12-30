cc8e2bbfd1f8215d790ad16246aba1fa3b8a9ae6
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-12-30 08:49:25 +0100

    cmake/rpath_auto.cmake: Add MACOSX_RPATH, use ../${CMAKE_INSTALL_LIBDIR} in rpath

--- cmake/rpath_auto.cmake
+++ cmake/rpath_auto.cmake
@@ -2,11 +2,14 @@ Function(cmake_helpers_rpath_auto target)
   if(TARGET ${target})
     get_target_property(_cmake_helpers_rpath_auto_target_type ${target} TYPE)
     if(
-        (_cmake_helpers_rpath_auto_target_type STREQUAL "STATIC_LIBRARY") OR
         (_cmake_helpers_rpath_auto_target_type STREQUAL "MODULE_LIBRARY") OR
         (_cmake_helpers_rpath_auto_target_type STREQUAL "SHARED_LIBRARY") OR
         (_cmake_helpers_rpath_auto_target_type STREQUAL "EXECUTABLE")
-      )       
+      )
+      #
+      # Although this is true by default since CMP0042, set it anyway on macOS or iOS
+      #
+      cmake_helpers_call(set_target_properties ${target} PROPERTIES MACOSX_RPATH TRUE)
       #
       # C.f. https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/RPATH-handling
       #
@@ -25,11 +28,16 @@ Function(cmake_helpers_rpath_auto target)
       # Set install rpath on platforms that support this feature
       # C.f. https://gitlab.kitware.com/vtk/vtk/-/blob/master/CMake/vtkModule.cmake
       #
+      # On Windows, dlls goes to CMAKE_HELPERS_INSTALL_BINDIR, else they go to CMAKE_HELPERS_INSTALL_LIBDIR
+      #
       if(UNIX)
+        #
+        # UNIX is true also on UNIX-like OSes, e.g. APPLE and CYGWIN
+        #
         if(APPLE)
-          set(_cmake_helpers_library_rpath_prefix "@loader_path")
+          set(_cmake_helpers_library_rpath_prefix "@loader_path/../${CMAKE_INSTALL_LIBDIR}")
         else()
-          set(_cmake_helpers_library_rpath_prefix "$ORIGIN")
+          set(_cmake_helpers_library_rpath_prefix "$ORIGIN/../${CMAKE_INSTALL_LIBDIR}")
         endif()
         cmake_helpers_call(set_property TARGET ${target} APPEND PROPERTY INSTALL_RPATH ${_cmake_helpers_library_rpath_prefix})
       endif()
